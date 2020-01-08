ee5067c9424fd1194876ea5c85a0c3e8cb28f4ea
Cesar Gomez <zootechdrum@gmail.com>
2020-01-07 17:48:12 -0800

    added card component to app file'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-list/card.list.component';
+import { promised } from '../node_modules/@types/q';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters:[]
+    }
+  }
+
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({monsters:users}))
+  }
+
+  render() {
+    return className="App">
+     <CardList name={yuha}></CardList>
+     {this.state.monsters.map(monster => (
+       <h1 key={promised.id}>{monser.name}</h1>
+     ))}
+  }
+
 }
 
 export default App;
---
+++ src/components/card-list/card.list.component.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+export const CardList = (props) => {
+<div></div>}
